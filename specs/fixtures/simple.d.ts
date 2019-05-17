de7d4aa505f69afe23c183f2a64362ad5188071f
RyoNkmr <ryonakamuraryo@gmail.com>
2019-05-17 18:43:37 +0900

    add test

---
+++ specs/fixtures/simple.d.ts
@@ -0,0 +1,23 @@
+type TypeAlias = number
+type Piyo = number | string
+type Foo = {
+  foo: Array<string>
+  bar: number[]
+}
+type Fuga = {
+  address: string
+  foo?: Foo | null
+  hoge?: {
+    fuga?: string
+  }
+  ho: Hoge
+}
+interface Hoge {
+  name: string
+  id: number | string
+  hoge?: number
+  fuga: number | null
+  fuga2: number | undefined
+  n: null
+  u: undefined
+}
---
+++ specs/typeParser.spec.ts
@@ -0,0 +1,78 @@
+import { initializeEnvironment, scan } from "../src/typeParser"
+
+describe("simpleCase", () => {
+  test("Answer to the Ultimate Question of Life, the Universe, and Everything", () => {
+    const { typeChecker, manager, source } = initializeEnvironment(
+      "./specs/fixtures/simple.d.ts"
+    )
+    scan(typeChecker, manager)(source)
+
+    const expected = {
+      TypeAlias: {
+        types: ["number"],
+      },
+      Piyo: {
+        types: ["number", "string"],
+      },
+      Foo: {
+        props: {
+          foo: {
+            types: ["string[]"],
+          },
+          bar: {
+            types: ["number[]"],
+          },
+        },
+      },
+      Fuga: {
+        props: {
+          address: {
+            types: ["string"],
+          },
+          foo: {
+            types: ["Foo", "null", "undefined"],
+          },
+          hoge: {
+            types: [
+              {
+                fuga: {
+                  types: ["string", "undefined"],
+                },
+              },
+              "undefined",
+            ],
+          },
+          ho: {
+            types: ["Hoge"],
+          },
+        },
+      },
+      Hoge: {
+        props: {
+          name: {
+            types: ["string"],
+          },
+          id: {
+            types: ["number", "string"],
+          },
+          hoge: {
+            types: ["number", "undefined"],
+          },
+          fuga: {
+            types: ["number", "null"],
+          },
+          fuga2: {
+            types: ["number", "undefined"],
+          },
+          n: {
+            types: ["null"],
+          },
+          u: {
+            types: ["undefined"],
+          },
+        },
+      },
+    }
+    expect(manager.getRegistry()).toEqual(expected)
+  })
+})
